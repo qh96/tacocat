05997d318e0a24acbc4566d5c8cdd6eb43ff1487
Hao Qin <qinhao@Hao.local>
2020-07-22 22:14:05 -0400

    add hit counter

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter)](https://hits.seeyoufarm.com)
 
 
 Tacocat
--- src/App.js
+++ src/App.js
@@ -33,10 +33,12 @@ function App() {
 <Container>
       
        <MultiSortTable></MultiSortTable>
-© 2020. All rights reserved.
+<p>© 2020. All rights reserved.  <a href="https://hits.seeyoufarm.com"/>
+    <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter"/></p>
 
+   
+    
     </Container>
-x
     </div>
    
   );
