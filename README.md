cb88e299e6818b94968d508cdf54ba40db015a03
Hao Qin <qinhao@Hao.local>
2020-07-22 22:19:03 -0400

    fix counter

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter)](https://hits.seeyoufarm.com)
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fqh96%2Ftacocat)](https://hits.seeyoufarm.com)
 
 
 Tacocat
--- src/App.js
+++ src/App.js
@@ -33,8 +33,7 @@ function App() {
 <Container>
       
        <MultiSortTable></MultiSortTable>
-<p>© 2020. All rights reserved.  <a href="https://hits.seeyoufarm.com"/>
-    <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fgjbae1212%2Fhit-counter"/></p>
+<p>© 2020. All rights reserved.  <a href="https://hits.seeyoufarm.com"/><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Feattacocat.herokuapp.com"/></p>
 
    
     
